c897388830d14fc47dc6b41e8fe0c2ee4c4461e3
cainiaokonhjian <292601787@qq.com>
2022-08-31 15:43:20 +0800

    fix:pass pteprint

--- kernel/vm.c
+++ kernel/vm.c
@@ -440,3 +440,33 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+void 
+printpagetable(pagetable_t pagetable,int level){
+    for(int i=0;i<512;i++){
+        pte_t pte = pagetable[i];
+        if((pte & PTE_V) && (pte & (PTE_R|PTE_W|PTE_X)) == 0){
+            uint64 child = PTE2PA(pte);
+            for(int j=0;j<=level;j++){
+                if(j != level)printf(".. ");
+                else printf("..");
+            }
+            printf("%d: pte %p pa %p\n",i,pte,child);
+            printpagetable((pagetable_t)child,level + 1);
+        } else if(pte & PTE_V){
+            uint64 child = PTE2PA(pte);
+            for(int j=0;j<=level;j++){
+                if(j != level)printf(".. ");
+                else printf("..");
+            }
+            printf("%d: pte %p pa %p\n",i,pte,child);
+      }
+    }
+}
+
+void
+vmprint(pagetable_t pagetable)
+{
+  printf("page table %p\n",pagetable);
+  printpagetable(pagetable,0);
+}
